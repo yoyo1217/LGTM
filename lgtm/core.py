2c8c75a4cd2fd24e2284e555bd278a689d51be97
yoyo1217 <youhei9753@gmail.com>
2020-03-01 13:34:08 +1100

    accept command line arguments

--- lgtm/core.py
+++ lgtm/core.py
@@ -2,8 +2,11 @@ import click
 
 
 @click.command()
-def cli():
-    lgtm()
+@click.option('--message', '-m', default='LGMT',
+              show_default=True, help='画像に載せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
+    lgtm(keyword, message)
     click.echo('lgtm')
 
 
